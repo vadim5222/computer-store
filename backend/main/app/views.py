0c039ac1906eee67710e0b63e4edec0db6239b5a
Vadim Kuznetsov <vadimkuznetsov1132@yandex.ru>
2026-07-01 19:14:35 +0300

    created register and login logic on backend

--- backend/main/app/views.py
+++ backend/main/app/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
+from .seralizers import *
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from django.conf import settings
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            refresh.payload.update({
+                'user_id':user.id,
+                'username':user.username
+            })
+            response = Response({
+                'refresh_token': str(refresh),
+                'access_token':str(refresh.access_token)
+            })
+            response.set_cookie(
+                'refresh_token',
+                str(refresh),
+                httponly= True,
+                max_age=settings.SIMPLE_JWT.get('REFRESH_TOKEN_LIFETIME'),
+                samesite='Lax'
+            )
+            response.set_cookie(
+                'access_token',
+                str(refresh.access_token),
+                httponly= True,
+                max_age=settings.SIMPLE_JWT.get('ACCESS_TOKEN_LIFETIME'),
+                samesite='Lax'
+            )
+            return response
+        return Response(serializer.errors)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        data = request.data
+        username = data.get('username')
+        password = data.get('password')
+        if username is None or password is None:
+            return Response({'error':'Логин и пароль обазательны'})
+        user = authenticate(username = username, password=password)
+        if user is None:
+            return Response({'error':'Такого пользователя не существует'})
+        refresh = RefreshToken.for_user(user)
+        refresh.payload.update({
+            'user_id':user.id,
+            'username':user.username
+        })
+        response = Response({
+            'success':'Вход выполнен успешно',
+            'refresh_token':str(refresh),
+            'access_token':str(refresh.access_token)
+        })
+        response.set_cookie(
+                'refresh_token',
+                str(refresh),
+                httponly= True,
+                max_age=settings.SIMPLE_JWT.get('REFRESH_TOKEN_LIFETIME'),
+                samesite='Lax'
+            )
+        response.set_cookie(
+                'access_token',
+                str(refresh.access_token),
+                httponly= True,
+                max_age=settings.SIMPLE_JWT.get('ACCESS_TOKEN_LIFETIME'),
+                samesite='Lax'
+            )
+        return response
+    
--- backend/main/main/settings.py
+++ backend/main/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'app',
 ]
 
@@ -120,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+AUTH_USER_MODEL = 'app.Users'
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
