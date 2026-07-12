4ef00da9cb4b2d4ba2a45eebe1965778452b2151
Vadim Kuznetsov <vadimkuznetsov1132@yandex.ru>
2026-07-12 20:48:03 +0300

    chore: ongoing authentication work (401 still broken)

--- frontend/client/src/main.jsx
+++ frontend/client/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage.jsx'
+import RegisterPage from './pages/RegisterPage.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
